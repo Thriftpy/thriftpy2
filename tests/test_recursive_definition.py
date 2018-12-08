091bb7b15e7aa1413b7388e9d1fef3acc7b3f105
Tzuhsing Gwo <lambda.guo@gmail.com>
2018-12-08 23:03:01 +0800

    Fix incorrect file path in tests

--- tests/test_recursive_definition.py
+++ tests/test_recursive_definition.py
@@ -4,7 +4,7 @@ from thriftpy.parser import load
 
 
 def test_constants():
-    thrift = load('parser-cases/recursive_definition.thrift')
+    thrift = load('./recursive_definition.thrift')
     assert thrift.Bar.thrift_spec == {1: (12, 'test', thrift.Foo, False)}
     assert thrift.Foo.thrift_spec == {
         1: (12, 'test', thrift.Bar, False), 2: (8, 'some', False)}
