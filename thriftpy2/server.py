507101aac3564b553eb40dbbfa9dfaceb7327d31
An Long <aisk1988@gmail.com>
2019-06-16 15:50:14 +0800

    Support kwargs style parameters passing in TSimpleServer

--- thriftpy2/server.py
+++ thriftpy2/server.py
@@ -37,8 +37,8 @@ class TServer(object):
 class TSimpleServer(TServer):
     """Simple single-threaded server that just pumps around one transport."""
 
-    def __init__(self, *args):
-        TServer.__init__(self, *args)
+    def __init__(self, *args, **kwargs):
+        TServer.__init__(self, *args, **kwargs)
         self.closed = False
 
     def serve(self):
