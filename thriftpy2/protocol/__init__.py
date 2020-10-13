d0a9ffb861e63bc69c3e07c722442fb11a3c3075
Jonathan Mackenzie <jon.mackenzie@youramigo.com>
2020-10-13 18:02:38 +1030

    Removed trailing comma
    (cherry picked from commit 4dcff996a5466370f9eb2de670119b12629ffe13)

--- thriftpy2/protocol/__init__.py
+++ thriftpy2/protocol/__init__.py
@@ -26,4 +26,4 @@ __all__ = ['TBinaryProtocol', 'TBinaryProtocolFactory',
            'TJSONProtocol', 'TJSONProtocolFactory',
            'TApacheJSONProtocol', 'TApacheJSONProtocolFactory',
            'TMultiplexedProtocol', 'TMultiplexedProtocolFactory',
-           'TCompactProtocol', 'TCompactProtocolFactory', ]
+           'TCompactProtocol', 'TCompactProtocolFactory']
