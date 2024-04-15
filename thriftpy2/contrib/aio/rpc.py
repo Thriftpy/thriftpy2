32169ffcd35201b819eaa4b80f5f9af865305f97
cocolato <haiizhu@outlook.com>
2024-04-15 21:42:10 +0800

    ipv6 support

--- thriftpy2/contrib/aio/rpc.py
+++ thriftpy2/contrib/aio/rpc.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import socket
 import urllib
 import warnings
 
@@ -17,7 +18,7 @@ async def make_client(service, host='localhost', port=9090, unix_socket=None,
                       cafile=None, ssl_context=None,
                       certfile=None, keyfile=None,
                       validate=True, url='',
-                      socket_timeout=None):
+                      socket_timeout=None, socket_family=socket.AF_INET):
     if socket_timeout is not None:
         warnings.warn(
             "The 'socket_timeout' argument is deprecated. "
@@ -30,22 +31,31 @@ async def make_client(service, host='localhost', port=9090, unix_socket=None,
         host = parsed_url.hostname or host
         port = parsed_url.port or port
     if unix_socket:
-        socket = TAsyncSocket(unix_socket=unix_socket,
-                              connect_timeout=connect_timeout,
-                              socket_timeout=timeout)
+        client_socket = TAsyncSocket(
+            unix_socket=unix_socket,
+            connect_timeout=connect_timeout,
+            socket_timeout=timeout,
+        )
         if certfile:
             warnings.warn("SSL only works with host:port, not unix_socket.")
     elif host and port:
-        socket = TAsyncSocket(
-            host, port,
-            socket_timeout=timeout, connect_timeout=connect_timeout,
-            cafile=cafile, ssl_context=ssl_context,
-            certfile=certfile, keyfile=keyfile, validate=validate)
+        client_socket = TAsyncSocket(
+            host,
+            port,
+            socket_timeout=timeout,
+            connect_timeout=connect_timeout,
+            cafile=cafile,
+            ssl_context=ssl_context,
+            certfile=certfile,
+            keyfile=keyfile,
+            validate=validate,
+            socket_family=socket_family,
+        )
     else:
         raise ValueError("Either host/port or unix_socket"
                          " or url must be provided.")
 
-    transport = trans_factory.get_transport(socket)
+    transport = trans_factory.get_transport(client_socket)
     protocol = proto_factory.get_protocol(transport)
     await transport.open()
     return TAsyncClient(service, protocol)
@@ -56,7 +66,8 @@ def make_server(service, handler,
                 proto_factory=TAsyncBinaryProtocolFactory(),
                 trans_factory=TAsyncBufferedTransportFactory(),
                 client_timeout=3000, certfile=None,
-                keyfile=None, ssl_context=None, loop=None):
+                keyfile=None, ssl_context=None, loop=None,
+                socket_family=socket.AF_INET):
     processor = TAsyncProcessor(service, handler)
 
     if unix_socket:
@@ -65,9 +76,14 @@ def make_server(service, handler,
             warnings.warn("SSL only works with host:port, not unix_socket.")
     elif host and port:
         server_socket = TAsyncServerSocket(
-            host=host, port=port,
+            host=host,
+            port=port,
             client_timeout=client_timeout,
-            certfile=certfile, keyfile=keyfile, ssl_context=ssl_context)
+            certfile=certfile,
+            keyfile=keyfile,
+            ssl_context=ssl_context,
+            socket_family=socket_family,
+        )
     else:
         raise ValueError("Either host/port or unix_socket must be provided.")
 
