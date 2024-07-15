07037f899ae8e11d6c42bbabbd058576b3fe1fe5
cocolato <haiizhu@outlook.com>
2024-07-15 22:29:40 +0800

    remove skip on windows

--- tests/test_oneway.py
+++ tests/test_oneway.py
@@ -20,9 +20,8 @@ class TestOneway(object):
     oneway_thrift = thriftpy2.load("oneway.thrift")
 
     def setup_class(self):
-        ctx = multiprocess.get_context("fork")
         server = make_server(self.oneway_thrift.echo, Dispatcher(), '127.0.0.1', 6000)
-        self.p = ctx.Process(target=server.serve)
+        self.p = multiprocess.Process(target=server.serve)
         self.p.start()
         time.sleep(1)  # Wait a second for server to start.
 
