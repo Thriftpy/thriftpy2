fd2bfe81d5cacb306b6943581dc18619ea1f309f
wangguang.1 <wangguang.1@bytedance.com>
2025-06-11 10:42:25 +0800

    fix memory leak risk

--- thriftpy2/protocol/cybin/cybin.pyx
+++ thriftpy2/protocol/cybin/cybin.pyx
@@ -245,9 +245,11 @@ cdef inline c_read_binary(CyTransportBase buf, int32_t size):
 
     if size > STACK_STRING_LEN:
         data = <char*>malloc(size)
-        buf.c_read(size, data)
-        py_data = data[:size]
-        free(data)
+        try:
+            buf.c_read(size, data)
+            py_data = data[:size]
+        finally:
+            free(data)
     else:
         buf.c_read(size, string_val)
         py_data = string_val[:size]
