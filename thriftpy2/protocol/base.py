40619116e624af37661ecdce137f71d201b070bc
An Long <aisk@users.noreply.github.com>
2026-07-20 23:48:27 +0900

    Type protocol and transport contracts (#376)

--- thriftpy2/protocol/base.py
+++ thriftpy2/protocol/base.py
@@ -1,15 +1,21 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Any, Tuple
+
 try:
     from typing import Protocol
 except ImportError:
     from typing_extensions import Protocol
 
+if TYPE_CHECKING:
+    from ..thrift import TPayload
+    from ..transport.base import TTransportBase
+
 
 class TProtocolFactory(Protocol):
     """Protocol factory interface for type annotations."""
 
-    def get_protocol(self, trans) -> TProtocolBase:
+    def get_protocol(self, trans: TTransportBase) -> TProtocolBase:
         """Return a protocol instance for the given transport."""
         ...
 
@@ -17,26 +23,26 @@ class TProtocolFactory(Protocol):
 class TProtocolBase(object):
     """Base class for Thrift protocol layer."""
 
-    def __init__(self, trans):
+    def __init__(self, trans: TTransportBase) -> None:
         self.trans = trans  # transport is public and used by TClient
 
-    def skip(self, ttype):
+    def skip(self, ttype: int) -> None:
         raise NotImplementedError
 
-    def read_message_begin(self):
+    def read_message_begin(self) -> Tuple[str, int, int]:
         raise NotImplementedError
 
-    def read_message_end(self):
+    def read_message_end(self) -> None:
         raise NotImplementedError
 
-    def write_message_begin(self, name, ttype, seqid):
+    def write_message_begin(self, name: str, ttype: int, seqid: int) -> None:
         raise NotImplementedError
 
-    def write_message_end(self):
+    def write_message_end(self) -> None:
         raise NotImplementedError
 
-    def read_struct(self, obj):
+    def read_struct(self, obj: TPayload) -> Any:
         raise NotImplementedError
 
-    def write_struct(self, obj):
+    def write_struct(self, obj: TPayload) -> None:
         raise NotImplementedError
