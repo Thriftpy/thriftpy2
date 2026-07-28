29e05a213d1edb2429042d02ced1b29c0522a87a
An Long <aisk1988@gmail.com>
2026-07-29 01:20:06 +0900

    Bump version 0.7.0alpha1

--- thriftpy2/__init__.py
+++ thriftpy2/__init__.py
@@ -3,7 +3,7 @@ import sys
 from .hook import install_import_hook, remove_import_hook
 from .parser import load, load_module, load_fp
 
-__version__ = '0.6.0'
+__version__ = '0.7.0a1'
 __python__ = sys.version_info
 __all__ = ["install_import_hook", "remove_import_hook", "load", "load_module",
            "load_fp"]
